0c920f405aaa3d1b0caf1f1e353b9520faf3ca2a
Nikolay Aleksandrov <nikolay.aleksandrov@rt.ru>
2023-06-14 14:47:39 +0300

    add PrefixAndPostfix.java

---
+++ src/main/java/Shield/Chapter1/arithmeticOperations/PrefixAndPostfix.java
@@ -0,0 +1,21 @@
+package Shield.Chapter1.arithmeticOperations;
+
+/**
+ * ++х; // префиксная форма - y=++x - сначала идет инкремент, потом присвоение
+ * или так:
+ * х++; // постфиксная форма - y=x++ - сначала идет присвоение, потом инкремент
+ */
+public class PrefixAndPostfix {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a++;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("=========");
+        a = 5;
+        b = ++a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
